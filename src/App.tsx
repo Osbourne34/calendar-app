304828c113884b8d829bffeb4ae75560387ff766
Evgeniy <evolkov1804@gmail.com>
2024-05-22 01:07:23 +0500

    prev days

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import { clsx } from "clsx";
 import { useState } from "react";
 
+const weekDays = [
+  "Воскресенье",
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+];
+
 export const App = () => {
   const [date, setDate] = useState(new Date());
   return (
@@ -20,54 +30,62 @@ const Calendar = (props: CalendarProps) => {
   const currentYear = date.getFullYear();
   const currentMonth = date.getMonth();
 
+  // Количество дней в месяце (МАЙ - 31)
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-  const days = Array(daysInMonth);
+  // День недели 1-го дня
+  const firstDayofMonth = new Date(currentYear, currentMonth, 7).getDay();
+  // День недели последнего дня
+  const lastDayofMonth = new Date(
+    currentYear,
+    currentMonth,
+    daysInMonth,
+  ).getDay();
+
+  const days = [];
+
+  for (let i = firstDayofMonth; i > 0; i--) {
+    const day = new Date(currentYear, currentMonth, -i + 1);
+    days.push(day);
+  }
 
   for (let i = 1; i <= daysInMonth; i++) {
     const day = new Date(currentYear, currentMonth, i);
-    days[i - 1] = day;
+    days.push(day);
+  }
+
+  console.log(lastDayofMonth);
+
+  for (let i = lastDayofMonth; i > 0; i--) {
+    const day = new Date(currentYear, currentMonth + 1, i);
+    console.log(day);
+
+    days.push(day);
   }
 
   return (
     <div className="container mx-auto px-5">
       <div className="flex items-center space-x-2">
         <button
+          className="rounded border p-2"
           onClick={() => setDate(new Date(currentYear, currentMonth - 1))}
-          className="border p-1"
         >
           Prev month
         </button>
-        <p>
-          {currentMonth + 1}.{currentYear}
-        </p>
+        <div>
+          {date.getMonth() + 1}.{date.getFullYear()}
+        </div>
         <button
+          className="rounded border p-2"
           onClick={() => setDate(new Date(currentYear, currentMonth + 1))}
-          className="border p-1"
         >
           Next month
         </button>
       </div>
 
       <div className="grid grid-cols-7">
-        {days.map((day, index) => {
-          const date = new Date();
-
-          return (
-            <div
-              key={index}
-              className={clsx("px-3 pb-5 pt-1", {
-                "bg-blue-500 text-white":
-                  +new Date(
-                    date.getFullYear(),
-                    date.getMonth(),
-                    date.getDate(),
-                  ) === +day,
-              })}
-            >
-              {day.getDate()}
-            </div>
-          );
-        })}
+        {days.map((date, index) => (
+          <div key={index}>{date.getDate()}</div>
+        ))}
       </div>
     </div>
   );
